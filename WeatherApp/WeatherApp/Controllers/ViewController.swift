e40c6c5a745beea1ebe3cb98829bf4c6ebbe51f3
Juan C <juanceballos@pursuit.org>
2020-01-31 15:26:40 -0500

    deleted files for programmatic ui setup

--- WeatherApp/WeatherApp/Controllers/ViewController.swift
+++ WeatherApp/WeatherApp/Controllers/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // enter a zipcode and display 7 Day Forecast
+    
+    // 3 controllers, main to enter zipcode and get collection view of weather forecast, city image with weather for day selected, favorite images tableview
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
